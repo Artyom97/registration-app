bd53ae89e8f9aad6a1d344a94f904f31863b4ef6
arantes15 <artyom@tidepoollabs.com>
2020-03-03 18:25:17 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import './components/global.css';
+import LoginTab from "./components/LoginTab";
+import Demo from './components/RegistrationTab';
+import Header from './components/Header';
+import UserPage from './components/UserPage';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+  render = () => (
+      <div>
+          <Header />
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path='/loginTab' component={LoginTab}/>
+                  <Route exact path='/registrationTab' component={Demo}/>
+                  <Route exact path='/userPage' component={UserPage}/>
+              </Switch>
+          </BrowserRouter>
+      </div>
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div>
+                <button className='LoginButton'>Login</button>
+            </div>
+            <div>
+                <button className='logoutButton'>Register</button>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
